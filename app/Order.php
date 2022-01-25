36801be6df9fe35caf05e991558e53f356ec02be
Fel <entica.fel@gmail.com>
2022-01-25 16:52:39 +0800

    [#9] Added comments

--- app/Order.php
+++ app/Order.php
@@ -14,7 +14,7 @@ class Order extends Model
     protected $fillable = [
         'user_id', 'shipping_fee', 'cart_total', 'address' 
     ];
-
+    
     public function user()
     {
         return $this->belongsTo(User::class, 'user_id');
--- app/User.php
+++ app/User.php
@@ -10,14 +10,43 @@ use Laravel\Passport\HasApiTokens;
 class User extends \TCG\Voyager\Models\User
 {
     use Notifiable, SoftDeletes, HasApiTokens;
-    
-    // protected $fillable = [
-    //     'name', 'email', 'password',
-    // ];
 
-    protected $hidden = [
-        'password', 'remember_token',
+    /*** MODEL CUSTOMIZABLE ATTRIBUTES ***/
+    
+    protected $table = 'users';
+    protected $primaryKey = 'id';
+    public $incrementing = true;
+    // protected $keyType = 'string'; // if the model primaryKey is not integer
+    // const CREATED_AT = 'creation_date'; // customization of column names
+    // const UPDATED_AT = 'updated_date';
+
+    protected $hidden = ['password', 'remember_token'];
+    // protected $visible = ['password', 'remember_token'];
+    
+ 
+
+    /** ELOQUENT MUTATORS **/
+
+    // protected $dateFormat = 'U';
+    // protected $dates = ['deleted_at']; // attributes mutated to dates and has instance of Carbon class
+    public $timestamps = true;
+   
+    public function getNameAttribute($value){ // computed accessor - allows to manipulate the return value
+        return ucfirst($value);
+    }
+    public function setNameAttribute($value) {  // mutator - allows and set manipulated value to eloquent model
+        $this->attributes['name'] = strtolower($value);
+    }
+    
+    /** ATRIBUTE CASTING - converting attributes to common data types */
+    protected $casts = [
+        // 'is_admin' => 'boolean',  // is_admin that's stored 0/1 in database will be converted
+        // 'options' => 'array', // ex accessing attribute that's stored as JSON will be deserialize as array, while serialize back to JSON when set
+        // 'created_at' => 'datetime:Y-m-d H:00', // customize the serialization format of individual Eloquent date attributes
     ];
+
+    /* model connection */
+    protected $connection = 'mysql';
 
     public function orders()
     {
